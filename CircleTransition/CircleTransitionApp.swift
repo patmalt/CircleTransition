508b94b987002952b45dea3df06f46f5d020106e
Patrick Maltagliati <patmalt@gmail.com>
2020-11-12 16:38:33 -0600

    Initial Commit

---
+++ CircleTransition/CircleTransitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  CircleTransitionApp.swift
+//  CircleTransition
+//
+//  Created by Patrick Maltagliati on 11/12/20.
+//
+
+import SwiftUI
+
+@main
+struct CircleTransitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
